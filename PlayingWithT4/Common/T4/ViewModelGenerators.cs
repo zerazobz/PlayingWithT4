5a37fc9a0ef24c5b276fd11858ac52253b78dc3c
Erick Asto Oblitas <erickastooblitas>
2016-08-16 08:14:40 -0500

    ->Partial advance templates

--- PlayingWithT4/Common/T4/ViewModelGenerators.cs
+++ PlayingWithT4/Common/T4/ViewModelGenerators.cs
@@ -1,19 +1,6 @@
 ﻿
 
-// This is the output code from your template
-// you only get syntax-highlighting here - not intellisense
-namespace MyNameSpace{
-  class MyFirstGeneratedClass{
-     public static void main (string[] args ){
-       System.Console.WriteLine("Hello, the time is now: 14/08/2016 12:09:06");
-            System.Console.Write(GetDataForTable("Rain"));
-         System.Console.Write(GetDataForTable("Temperatur"));
-         
-     }
-  static string GetDataForTable(string table){
-     // TODO - will be implemented later...
-     return "HA HA JA JA....";
-    }
-  }
-}
- 
+
+
+
+
